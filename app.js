7ad50c0797dbed9ceb1c49087b0d94d9277be4a6
Daniel Fischer <dev_dafi@icloud.com>
2021-07-07 15:45:45 +0200

    Refactor code and add functionality. Chaining callbacks to work with given coordinates. Remove hard coded location and use command line argument instead. Error handling.

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+// REQUIREMENTS
+
 require("dotenv").config();
-// const request = require("postman-request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-// geocode does not have a direct return value!
-geocode("Berlin", (error, data) => {
-    // access to results, after geocoding process is complete
-    console.log("Error:", error);
-    console.log("Data:", data);
 
-})
+//VARIABLES
 
-forecast(52.51667, 13.38333, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+const location = process.argv[2];
+
+if (!location) {
+    console.log("You have to provide a location via command line argument!");
+} else {
+    // gecode provides lat/lon to work with forecast
+    geocode(location, (error, data) => {
+
+        if (error) {
+            return console.log(error); //stops function execution, after log error to console
+        }
+
+        // data = success output from geocode. 
+        // data is defined as an object -> properties are accessable 
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(data.location);
+            console.log(forecastData);
+        })
+    })
+}
+
+
+
